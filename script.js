0b6320f6f591984478f22bee71fd739ace68ad4d
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-04-15 12:51:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 document.getElementById('learnMoreBtn').addEventListener('click', function() {
     document.querySelector('.intro').scrollIntoView({ behavior: 'smooth' });
 });
+document.querySelectorAll(".faq-question").forEach(button => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+
+    answer.style.display =
+      answer.style.display === "block" ? "none" : "block";
+  });
+});
